804f03acd9e1ee310d91ab67f086217f545b13b9
Elise Worthy <eworthy@bluebox.net>
2011-07-19 20:47:17 -0700

    Simplified the Microposts controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,11 +3,6 @@ class MicropostsController < ApplicationController
   # GET /microposts.xml
   def index
     @microposts = Micropost.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @microposts }
-    end
   end
 
   # GET /microposts/1
